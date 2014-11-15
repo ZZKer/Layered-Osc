909be040465f20ef899c6b7f8403a8ea4407aaa1
ZZKer <Kyle.Mumenthaler@gmail.com>
2014-11-15 14:45:29 -0600

    I'll fix this later if this works

--- index.js
+++ index.js
@@ -5,12 +5,13 @@
  * @desc Layers oscolations every x notes from top note to bottom note.
  */
  
- import tri from 'opendsp/osc';
+ //import tri from 'opendsp/osc';
  
  export function layOsc(t, topn, bottomn, x){
     var output = 0;
     for(var i=topn; i >= bottomn; i = i - x){
-      output = output + tri(t, i);
+      output = output + (Math.abs(1 - (2 * t * i) % 2) * 2 - 1);
+      //output = output + tri(t, i);
     }
     return output;
   }
