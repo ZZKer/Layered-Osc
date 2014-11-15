2cc770977e383e80c203667a2af1569f34d219b0
ZZKer <Kyle.Mumenthaler@gmail.com>
2014-11-15 13:31:29 -0600

    added note variation, comments

--- index.js
+++ index.js
@@ -2,14 +2,19 @@
 /**
  * @module Layered Osc
  * @author ZZKer
+ * @ desc Layers oscolations every x notes from top note to bottom note.
  */
  
- import { sin, saw, ramp, tri, sqr, noise } from 'opendsp/osc';
-
-  export function LOsc(top, bottom, type){
+ /**
+  * top - the top layered note
+  * bottom - the bottom layered note
+  * x - the variation when adding note layers
+  * type - the type of oscolation
+  */
+ export function LOsc(top, bottom, x, type){
     var output = 0;
-    for(var i=top; i >= bottom; i--) {
-      output = output + tri(t, i);
+    for(var i=top; i >= bottom; i -= x) {
+      output = output + type(t, i);
     }
     return output;
   }
--- test.js
+++ test.js
@@ -1,10 +1,24 @@
 
 /**
  * test
+ * this test uses triliniar as the oscolation,
+ * but can use anything, really.
  */
-import tri from 'opendsp/osc';
+import tri from 'opendsp/osc/index';
 import LOsc from './index';
 
+//top note
+var topn = 440;
+
+//bottom note
+var botn = 400;
+
+//note variation
+var x = 1;
+
+//volume
+var v = 0.1;
+
 export function dsp(t) {
-    return 01 * LOsc(440, 400, tri);
+    return v * LOsc(topn, botn, x, tri);
   }
